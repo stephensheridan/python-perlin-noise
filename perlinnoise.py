eb14b403f8872d54ad2f8354a5e4f472d36e01df
Stephen Sheridan <stesher@gmail.com>
2017-01-16 11:13:18 +0000

    Added interpolation range to perlin output values

--- perlinnoise.py
+++ perlinnoise.py
@@ -1,11 +1,14 @@
 """
 Author: Stephen Sheridan
 Simple 1D Perlin noise generator using 
-Caseman's Python Perlin noise gen lib
+Casey Duncan's Python Perlin noise gen lib
 https://github.com/caseman/noise
 """
 from noise import pnoise1
+from numpy import interp
 import matplotlib.pyplot as plt
+import sys
+import time
 
 # Pick a base point in time. We can move forward in time to 
 # geberate more perlin noise
@@ -17,14 +20,21 @@ step = 0.01
 pvalues = []
 # Generate N perlin noise values from base with time step
 N = 1000
+# Set the required output range for interpolation [MIN, MAX]
+outRange = [0,100]
 
 for x in range(N):
 	# Use 1d pnoise1 to generate noise value
-	pvalues.append(pnoise1(base))
+	pval = pnoise1(base)
+	# Store and move on
+	pvalues.append(interp(pval, [-1,1], outRange))
 	# Incerement base by step to get next noise value
 	base = base + step
 
 # Plot the values we got
 plt.plot(pvalues)
 plt.ylabel('Perlin Noise')
+plt.xlabel('Time Step')
 plt.show()
+
+
